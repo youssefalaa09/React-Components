b28469de6b40d06d595dd1ad0dbec6ec13955932
Youssef <127805548+youssefalaa09@users.noreply.github.com>
2023-10-03 11:46:43 +0300

    Add index file

---
+++ src/components/Content.js
@@ -0,0 +1,13 @@
+export default function Content(){
+    return(
+        <div>
+            <p>Some Info About React:</p>
+            <ul>
+                <li>React Fact1</li>
+                <li>React Fact2</li>
+                <li>React Fact3</li>
+                <li>React Fact4</li>
+            </ul>
+        </div>
+    );
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,5 @@
+export default function Navbar(){
+    return (
+        <img src={require("../react.png")} style={{width:"250px"}} alt="logo"/>
+    );
+}
